cef9e3677760752904f8a3be7f443cf7ff2cc54a
edison rosales <edisonrosalescampos@gmail.com>
2025-03-31 17:48:07 -0400

    actualizando link instagram fitosana

--- src/components/layout/HeaderSection.vue
+++ src/components/layout/HeaderSection.vue
@@ -3,7 +3,7 @@
       <div class="social">
         <ul>
           <li><a href="https://www.facebook.com/profile.php?id=61565835603289&mibextid=ZbWKwL" title="Facebook" target="_blank"><span><i class="fab fa-facebook-f fa-lg"></i></span></a></li>
-          <li><a href="https://www.instagram.com/distribuidor_fitosana.2024?igsh=MTg4dHViNmpleXQ3eg==" title="Instagram" target="_blank"><span><i class="fab fa-instagram fa-lg"></i></span></a></li>   
+          <li><a href="https://www.instagram.com/fitosana_jr?igsh=MTg4dHViNmpleXQ3eg==" title="Instagram" target="_blank"><span><i class="fab fa-instagram fa-lg"></i></span></a></li>   
           <li><a href="https://wa.me/584161974036" title="WhatsApp" target="_blank"><span><i class="fab fa-whatsapp fa-lg"></i></span></a></li>
         </ul>
       </div>
