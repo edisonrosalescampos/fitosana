6eae54af180f2c99f3db52e5ada9f09d8420e6b7
edison rosales <edisonrosalescampos@gmail.com>
2025-08-23 14:46:01 -0400

    actualizando copyright footer

--- src/components/layout/FooterSection.vue
+++ src/components/layout/FooterSection.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <div class="copyright">
       <div class="copy">
-        &copy; Distribuidor Fitosana 2025 | Todos los derechos reservados.
+        &copy; Suplementos Acarigua 2025 | Todos los derechos reservados.
       </div>
     </div>
   </div>
